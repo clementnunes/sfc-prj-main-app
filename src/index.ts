cb88468984dae937cf8b85f4abcdbecb3827273a
Nunes <clement.nunes@efrei.net>
2023-03-27 19:51:04 +0200

    f

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,8 @@ import {basicRoutes} from "./lib/basic-routes";
 import {usersRoutes} from "./lib/users-routes";
 import {DbConnector} from "./lib/db-connector";
 import {KafkaConfig} from "./config/kafka.config";
-import {Kafka} from "kafkajs";
-import fastifyKafkaJS from "fastify-kafkajs";
 
+const fastifyKafka = require("@fastify/kafka")
 
 /*const kafka = new Kafka({
     clientId: KafkaConfig.KAFKA_APP_NAME,
@@ -19,53 +18,27 @@ async function run() {
     const dbConn = DbConn.getInstance();
     await dbConn._appDataSource.initialize()
 
-
-    /*const producer = kafka.producer()
-
-    await producer.connect()
-    await producer.send({
-        topic: KafkaConfig.KAFKA_TOPIC,
-        messages: [
-            { value: "test" }
-        ]
-    })
-
-    await producer.disconnect()*/
+    await server
+        .register(fastifyKafka, {
+            producer: {
+                'metadata.broker.list': '127.0.0.1:9092',
+                'group.id': KafkaConfig.KAFKA_GROUP_NAME,
+                'fetch.wait.max.ms': 10,
+                'fetch.error.backoff.ms': 50,
+                'dr_cb': true
+            },
+            consumer: {
+                'metadata.broker.list': '127.0.0.1:9092',
+                'group.id': KafkaConfig.KAFKA_GROUP_NAME,
+                'fetch.wait.max.ms': 10,
+                'fetch.error.backoff.ms': 50,
+                'auto.offset.reset': 'earliest'
+            }
+        })
 
     await server.register(DbConnector)
     await server.register(usersRoutes)
     await server.register(basicRoutes);
-
-    await server.register(fastifyKafkaJS, {
-        clientConfig: {
-            brokers: [KafkaConfig.KAFKA_BOOTSTRAP_SERVER],
-            clientId: KafkaConfig.KAFKA_APP_NAME
-        },
-        consumers: [
-            {
-                consumerConfig: {
-                    groupId: KafkaConfig.KAFKA_APP_NAME
-                },
-                subscription: {
-                    topic: KafkaConfig.KAFKA_TOPIC,
-                    fromBeginning: false
-                },
-                runConfig: {
-                    eachMessage: async ({ message }) => {
-                        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-                        console.log(`Consumed message: ${message.value}`);
-                    }
-                }
-            }
-        ]
-    });
-
-    server.post('/produce', async (request) => {
-        return server.kafka.producer.send({
-            topic: KafkaConfig.KAFKA_TOPIC,
-            messages: [{ key: 'key1', value: "3" }]
-        });
-    });
 
     await server.listen({ port: FastifyConfig.FASTIFY_PORT, host: FastifyConfig.FASTIFY_ADDR })
 }
